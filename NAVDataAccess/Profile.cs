e17d45b76354c68956b8236267620eced9c082db
Kevin Moens <kmoens@lakeco.com>
2016-04-20 20:58:00 -0500

    initial .net code commit

---
+++ NAVDataAccess/Profile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAVDataAccess
+{
+    public class Profile
+    {
+        String _FirstName;
+        public String FirstName
+        {
+            get
+            {
+                return _FirstName;
+            }
+            set
+            {
+                _FirstName = value;
+            }
+        }
+        String _LastName;
+        public String LastName
+        {
+            get
+            {
+                return _LastName;
+            }
+            set
+            {
+                _LastName = value;
+            }
+        }
+    }
+}
